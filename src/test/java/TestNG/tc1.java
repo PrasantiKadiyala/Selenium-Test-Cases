2947860b8ceec8b6dbf2d686c94949c3e20edfb1
Prasanti Kadiyala <prasanti.kadiyala@tcs.com>
2021-01-15 02:27:47 +0530

    Update tc1.java

--- src/test/java/TestNG/tc1.java
+++ src/test/java/TestNG/tc1.java
@@ -1,9 +1,14 @@
 package TestNG;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,15 +18,15 @@ public class tc1 {
 
    		@BeforeMethod
 		public void launch() {
-		  String chromeDriverPath = "/usr/bin/chromedriver";
-		  System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		  ChromeOptions options = new ChromeOptions();
+   		  String chromeDriverPath = "/usr/bin/chromedriver";
+  		  System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+  		  ChromeOptions options = new ChromeOptions();
   		  options.setBinary("/usr/bin/chromedriver");
   		  WebDriver driver = new ChromeDriver(options);
-		  
+  		  
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		  driver.get("http://18.223.210.194:32771/addressbook/");
+		  driver.get("http://15.207.98.49:32768/addressbook/");
 		
 		}
 		
@@ -43,15 +48,15 @@ public class tc1 {
 		 driver.close();  */
 	
 	 public static void main(String []args) throws MalformedURLException{
-        new DesiredCapabilities();
-            URL serverurl = new URL("http://localhost:9515");
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            WebDriver driver = new RemoteWebDriver(serverurl,capabilities);
-        driver.get("http://www.google.com");
-        WebElement searchEdit = driver.findElement(By.name("q"));
-        searchEdit.sendKeys("Selftechy on google");
-        searchEdit.submit();
+	        new DesiredCapabilities();
+	            URL serverurl = new URL("http://localhost:9515");
+	            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+	            WebDriver driver = new RemoteWebDriver(serverurl,capabilities);
+	        driver.get("http://www.google.com");
+	        WebElement searchEdit = driver.findElement(By.name("q"));
+	        searchEdit.sendKeys("Selftechy on google");
+	        searchEdit.submit();
 
-    }
+	    }
  }
   }
