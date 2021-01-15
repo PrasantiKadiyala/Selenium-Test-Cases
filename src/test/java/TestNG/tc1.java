82deba528b88647a4eeda688f2693b1c61673773
Prasanti Kadiyala <prasanti.kadiyala@tcs.com>
2021-01-15 13:35:35 +0530

    Update tc1.java

--- src/test/java/TestNG/tc1.java
+++ src/test/java/TestNG/tc1.java
@@ -1,19 +1,22 @@
 package TestNG;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class tc1 {
 	
-		
-	  @Test
-		
-	 public void verify() throws MalformedURLException{
+	  @Test		
+	  public void verify() throws MalformedURLException{
 	        new DesiredCapabilities();
 	            URL serverurl = new URL("http://localhost:9515");
 	            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
@@ -24,6 +27,4 @@ public class tc1 {
 	        searchEdit.submit();
 
 	    }
-	  
-
   }
